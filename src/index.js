9cda4a83d9bda6586d1cb2fd85d1abfe4e77d3aa
khalledalkarmi <khalled_95@hotmail.com>
2022-11-13 16:52:36 +0300

    Add auth kit router

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import "./index.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider authType={'cookie'}
+      authName={'_auth'}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
 
